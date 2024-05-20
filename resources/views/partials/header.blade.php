dc4e8bbf1b2a67e09e7e892cba33c2dd258a0724
Federica Gallo <153717958+Federicaye@users.noreply.github.com>
2024-05-20 17:31:12 +0200

    nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,16 +2,16 @@
     <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="Logo">
     <div>
         <ul class="d-flex align-items-center justify-content-center ">
-            <li>character</li>
-            <li>comics</li>
-            <li>movies</li>
-            <li>tv</li>
-            <li>games</li>
-            <li>collectibles</li>
-            <li>videos</li>
-            <li>fans</li>
-            <li>news</li>
-            <li>shop</li>
+            <li> <a class="{{ Route::currentRouteName() == 'character' ? 'active' : ''}}" href="{{route('charater')}}">character</a> </li>
+            <li> <a class="{{ Route::currentRouteName() == 'comics' ? 'active' : ''}}" href="{{route('comics')}}">comics</a> </li>
+            <li> <a class="{{ Route::currentRouteName() == 'movies' ? 'active' : ''}}" href="{{route('movies')}}"> movies </a> </li>
+            <li> <a class="{{ Route::currentRouteName() == 'tv' ? 'active' : ''}}" href="{{route('tv')}}">tv</a> </li>
+            <li> <a class="{{ Route::currentRouteName() == 'games' ? 'active' : ''}}" href="{{route('games')}}">games</a> </li>
+            <li> <a class="{{ Route::currentRouteName() == 'collectibles' ? 'active' : ''}}" href="{{route('collectibles')}}">collectibles</a> </li>
+            <li> <a class="{{ Route::currentRouteName() == 'videos' ? 'active' : ''}}" href="{{route('videos')}}">videos</a> </li>
+            <li> <a class="{{ Route::currentRouteName() == 'fans' ? 'active' : ''}}" href="{{route('fans')}}">fans</a> </li>
+            <li> <a class="{{ Route::currentRouteName() == 'news' ? 'active' : ''}}" href="{{route('news')}}">news</a> </li>
+            <li> <a class="{{ Route::currentRouteName() == 'shop' ? 'active' : ''}}" href="{{route('shop')}}">shop</a></li>
         </ul>
     </div>
     <input type="text">
